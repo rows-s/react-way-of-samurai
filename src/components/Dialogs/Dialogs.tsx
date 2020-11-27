f2b5584bda083624125f5f89032be426736d0e30
Vladimir <I3rovvser@gmail.com>
2020-11-27 18:29:58 +0300

    My step

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:src/components/Dialogs/Dialogs.tsx
 import React from 'react';
 import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
@@ -64,3 +65,46 @@ export default Dialogs;
 
 
 
+=======
+import React from 'react';
+import s from './Dialogs.module.css';
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
+
+const Dialogs = (props) => {
+
+    let state = props.dialogsPage;
+
+    let dialogsElements = state.dialogs.map( d => <DialogItem name={d.name} id={d.id} />  );
+    let messagesElements = state.messages.map( m => <Message message={m.message} /> );
+    let newMessageBody = state.newMessageBody;
+
+    let onSendMessageClick = () => {
+        props.sendMessage();
+    }
+
+    let onNewMessageChange = (e) => {
+        let body = e.target.value;
+        props.updateNewMessageBody(body);
+    }
+
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                { dialogsElements }
+            </div>
+            <div className={s.messages}>
+                <div>{ messagesElements }</div>
+                <div>
+                    <div><textarea value={newMessageBody}
+                                   onChange={onNewMessageChange}
+                                   placeholder='Enter your message'></textarea></div>
+                    <div><button onClick={onSendMessageClick}>Send</button></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Dialogs;
+>>>>>>> parent of bd3d020... 48: full copy for dialogs-reducer:src/components/Dialogs/Dialogs.jsx
